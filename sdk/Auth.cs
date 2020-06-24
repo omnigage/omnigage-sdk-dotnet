076ac6933220695f04a7df26b88a4e793a891cd0
Micheal Morgan <mail@michealmorgan.com>
2020-06-24 16:26:16 -0500

    Moving resources into appropriate namespaces

---
+++ sdk/Auth.cs
@@ -0,0 +1,26 @@
+﻿namespace Omnigage.Auth
+{
+    /// <summary>
+    /// Authentication and request context
+    /// </summary>
+    public class AuthContext
+    {
+        public string TokenKey { get; set; }
+        public string TokenSecret { get; set; }
+        public string Host { get; set; }
+        public string AccountKey { get; set; }
+
+        /// <summary>
+        /// Create Authorization token following RFC 2617 
+        /// </summary>
+        /// <returns>Base64 encoded string</returns>
+        public string Authorization
+        {
+            get
+            {
+                byte[] authBytes = System.Text.Encoding.UTF8.GetBytes($"{this.TokenKey}:{this.TokenSecret}");
+                return System.Convert.ToBase64String(authBytes);
+            }
+        }
+    }
+}
