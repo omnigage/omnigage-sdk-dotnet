0e5312c8e461db5513335a096cc5b7af4eaaa716
Micheal Morgan <mail@michealmorgan.com>
2020-07-06 16:04:25 -0500

    Adding email, email id, and email message resources with test coverage

---
+++ sdk/src/Resources/EmailId.cs
@@ -0,0 +1,12 @@
+﻿using Omnigage.Util;
+
+namespace Omnigage.Resources
+{
+    /// <summary>
+    /// Resource: `/email-ids` - https://omnigage.docs.apiary.io/#reference/identity-resources/email-id-collection/retrieve-email-messages
+    /// </summary>
+    public class EmailIdResource : Adapter
+    {
+        public override string Type { get; } = "email-ids";
+    }
+}
