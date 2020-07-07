7afdb8fa6c28db5f8a63c1cfdd417b620a6ba3e4
Micheal Morgan <mail@michealmorgan.com>
2020-07-07 17:01:20 -0500

    Expanding conference resource

--- sdk/src/Resources/Conference.cs
+++ sdk/src/Resources/Conference.cs
@@ -1,4 +1,5 @@
-﻿using Omnigage.Util;
+﻿using Newtonsoft.Json;
+using Omnigage.Util;
 
 namespace Omnigage.Resources
 {
@@ -8,5 +9,22 @@ namespace Omnigage.Resources
     public class ConferenceResource : Adapter
     {
         public override string Type { get; } = "conferences";
+
+        public string Label { get; set; }
+
+        [JsonProperty(propertyName: "beep-on-enter")]
+        public bool BeepOnEnter { get; set; }
+
+        [JsonProperty(propertyName: "beep-on-exit")]
+        public bool BeepOnExit { get; set; }
+
+        [JsonProperty(propertyName: "play-hold-music")]
+        public bool PlayHoldMusic { get; set; }
+
+        [JsonProperty(propertyName: "max-participants")]
+        public string MaxParticipants { get; set; }
+
+        [JsonProperty(propertyName: "is-muted")]
+        public bool IsMuted { get; set; }
     }
 }
